5631ad0c74d95b64fe5e510f26cce4a8187d4aa9
Olivier Cailloux <olivier.cailloux@gmail.com>
2020-04-01 16:54:52 +0200

    time, not amount

--- src/main/java/io/github/oliviercailloux/samples/coffee/DripCoffeeMaker.java
+++ src/main/java/io/github/oliviercailloux/samples/coffee/DripCoffeeMaker.java
@@ -4,7 +4,7 @@ package io.github.oliviercailloux.samples.coffee;
  * A <a href=
  * "https://www.startpage.com/sp/search?query=drip+coffee+maker&cat=pics">drip
  * coffee maker</a>. It uses a specific brand of coffee, which makes it able to
- * produce coffee of any strength from 0 to 10. It takes a constant amount of 2
+ * produce coffee of any strength from 0 to 10. It takes a constant time of 2
  * minutes to produce coffee (of strength higher than zero). The energy required
  * for producing a coffee (of strength higher than zero) is an estimated 83 watt
  * hours.
