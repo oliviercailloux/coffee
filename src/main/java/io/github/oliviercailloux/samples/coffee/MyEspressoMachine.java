ee94ea18098a73bacf1cdf965d55dc684db37c5a
Olivier Cailloux <olivier.cailloux@gmail.com>
2021-03-01 21:56:10 +0100

    If s ≠ 0

--- src/main/java/io/github/oliviercailloux/samples/coffee/MyEspressoMachine.java
+++ src/main/java/io/github/oliviercailloux/samples/coffee/MyEspressoMachine.java
@@ -2,8 +2,8 @@ package io.github.oliviercailloux.samples.coffee;
 
 /**
  * A specific espresso machine, that produces coffee of strength up to 20, whose
- * power is 2000 watts, and that produces a coffee of strength <em>s</em> in
- * <em>140 + 2 * s</em> seconds.
+ * power is 2000 watts, and that produces a coffee of strength <em>s</em> (if
+ * <em>s</em> ≠ 0) in <em>140 + 2 * s</em> seconds.
  *
  */
 public class MyEspressoMachine implements EspressoMachine {
